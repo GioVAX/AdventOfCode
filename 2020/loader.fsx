0908214a7fee8c8e6e6da0b59371becee88c39d8
GioVAX <giovannipezzino.64@gmail.com>
2020-11-25 18:27:39 +0100

    2020 - preparation

---
+++ 2020/loader.fsx
@@ -0,0 +1,12 @@
+#r "nuget:FsUnit.Xunit"
+#r "nuget:Xunit"
+
+// let modules = fsi.CommandLineArgs
+
+// for m in modules do
+//     #load (m+".fs")
+//     open m
+
+// #if INTERACTIVE
+// open Tests
+// #endif
