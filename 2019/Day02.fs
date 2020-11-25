9085b5903ccc31a0ec273f8c5f4213e95f758778
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2020-11-25 22:09:58 +0100

    Copied Template to 2019

---
+++ 2019/Day02.fs
@@ -0,0 +1,22 @@
+module Day02
+
+open FsUnit.Xunit
+open Xunit
+
+let data = []
+
+
+let result1 =
+    1
+    
+let result2 =
+    2
+
+[<Theory>]
+[<InlineData(12, 2)>]
+let ``Given a mass SHOULD compute correct fuel`` (mass, expected) =
+    result1 |> should equal 1
+
+[<Fact>]
+let ``Simple test`` () =
+    result2 |> should equal 2
