e4ca997f062c640d92ea3ec9c6bf78ead5c95e31
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2020-12-04 09:42:43 +0100

    Day04 Prep

---
+++ 2020/Day04.fs
@@ -0,0 +1,10 @@
+module Day04
+
+let data = []
+
+
+let result1 =
+    1
+    
+let result2 =
+    2
---
+++ 2020/Day04Tests.fs
@@ -0,0 +1,15 @@
+module Day04Tests
+
+open FsUnit.Xunit
+open Xunit
+
+open Day04
+
+[<Fact>]
+let ``Test case 1`` () =
+    (1 |> should equal 1)
+
+[<Theory>]
+[<InlineData(12, 2)>]
+let ``Test case 2`` (p1, p2) =
+    [] |> should matchList []
