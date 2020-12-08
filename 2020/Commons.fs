c09d41b8a4843e0228b17b81f95e18cfa4315c99
GioVAX <giovannipezzino.64@gmail.com>
2020-12-08 17:08:05 +0100

    Added Regex active pattern

--- 2020/Commons.fs
+++ 2020/Commons.fs
@@ -1,5 +1,12 @@
 module Commons
 
+open System.Text.RegularExpressions
+
+let (|Regex|_|) pattern input =
+    let m = Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [for g in m.Groups -> g.Value])
+    else None
+
 let groupDataOnSeparator (groupSeparator:string) (separator:string) =
     let rec loop = function 
     | [] -> []
