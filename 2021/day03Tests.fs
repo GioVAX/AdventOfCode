f3c20500fd62eb77be7884f0f2caabc95a50f632
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2021-12-12 22:20:48 +0100

    day 3 part 1 prep

---
+++ 2021/day03Tests.fs
@@ -0,0 +1,15 @@
+module Day03Tests
+
+open FsUnit.Xunit
+open Xunit
+
+open Day03
+
+let testInput = 
+    [ "00100"; "11110"; "10110"; "10111"; "10101"; "01111"; "00111"; "11100"; "10000"; "11001"; "00010"; "01010"; ]
+
+[<Fact>]
+let ``solvePart1 with the testInput SHOULD return 198`` () =
+    testInput
+    |> solvePart1
+    |> should equal 198
