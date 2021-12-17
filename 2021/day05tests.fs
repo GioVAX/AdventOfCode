51d6ce218c8728927f4d2a935f9af18e554d7fa7
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2021-12-17 19:09:31 +0100

    day 5 part 1 prep

---
+++ 2021/day05tests.fs
@@ -0,0 +1,25 @@
+module Day05Tests
+
+open FsUnit.Xunit
+open Xunit
+
+open Day05
+
+let testInput = 
+    [
+        "0,9 -> 5,9";
+        "8,0 -> 0,8";
+        "9,4 -> 3,4";
+        "2,2 -> 2,1";
+        "7,0 -> 7,4";
+        "6,4 -> 2,0";
+        "0,9 -> 2,9";
+        "3,4 -> 1,4";
+        "0,0 -> 8,8"; 
+        "5,5 -> 8,2"; 
+    ]
+
+[<Fact>]
+let ``parse first test item SHOULD return the expected result`` () =
+    let result = parse "0,9 -> 5,9"
+    result |> should equal (Point(0,9), Point(5,9))
