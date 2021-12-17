4b44c58a406fb8676829ee3eadd82ac074cdf789
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2021-12-17 18:53:14 +0100

    utils module

---
+++ 2021/Utils.fs
@@ -0,0 +1,6 @@
+module Utils
+
+let (|Regex|_|) pattern input =
+    let m = System.Text.RegularExpressions.Regex.Match(input, pattern)
+    if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
+    else None
