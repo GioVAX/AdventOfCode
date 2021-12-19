1caf25e12ba7db891b11b0be8588340e68590814
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2021-12-19 17:42:26 +0100

    day 6 part 1 complete

--- 2021/day06tests.fs
+++ 2021/day06tests.fs
@@ -10,4 +10,10 @@ let testInput = "3,4,3,1,2"
 [<Fact>]
 let ``parse testInput  should produce the correct result`` () =
     parse testInput
-    |> should equal [|3;4;3;1;2|]
+    |> should equal [3;4;3;1;2]
+
+[<Fact>]
+let ``solvePart1 with testInput SHOULD result 5934`` () =
+    solvePart1 testInput
+    |> should equal 5934
+
