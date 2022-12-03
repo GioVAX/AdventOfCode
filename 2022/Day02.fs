5f18346a1125d2fb18dc67fdd48c6b7e0eba72b7
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2022-12-04 00:35:56 +0100

    day02 - part1

--- 2022/Day02.fs
+++ 2022/Day02.fs
@@ -1,4 +1,18 @@
 module Day02
 
+let score = function
+    | "A X" -> 4
+    | "A Y" -> 8
+    | "A Z" -> 3
+    | "B X" -> 1
+    | "B Y" -> 5
+    | "B Z" -> 9
+    | "C X" -> 7
+    | "C Y" -> 2
+    | "C Z" -> 6
+    | _ -> failwith "wrong combo"
+
 let part1 lines =
-    15
+    lines
+    |> List.map score
+    |> List.sum
