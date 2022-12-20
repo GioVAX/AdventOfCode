7ce602d9713961fed80d5e7a24a0ebc16929d813
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2022-12-20 23:10:17 +0100

    Day 5 - setup

---
+++ 2022/Day05.fs
@@ -0,0 +1,1 @@
+module Day05
---
+++ 2022/Day05Tests.fs
@@ -0,0 +1,12 @@
+module Day05Tests
+
+open FsUnit.Xunit
+open Xunit
+
+open Day05
+
+let testInput = Utils.readData "..\\..\\..\\Day05InputTest.txt"
+
+[<Fact>]
+let ``convert 1`` () =
+    ()
