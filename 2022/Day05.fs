098c615f0b5f5c8174cd8e0c6131ffa885364958
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2023-12-18 14:18:22 +0100

    day 5

--- 2022/Day05.fs
+++ 2022/Day05.fs
@@ -1,1 +1,29 @@
 module Day05
+
+open System.Text.RegularExpressions
+
+let endOfConfig numStacks =
+    let x = [for i in 1..numStacks -> " " + string i + " "]
+    System.String.Join(' ', x)
+
+let regexForConfig = "(   )|\\[([A-Z])]"
+
+let readConfigLine s =
+    Regex.Split(s, regexForConfig)
+    |> Array.chunkBySize 2
+    |> Array.filter (fun l -> l.Length = 2)
+    |> Array.map (fun l -> 
+        match l[1] with
+        | "   " -> ""
+        | s -> s)
+
+let initialConfigParser (lines: string list) numStacks : char list array =
+    let output = Array.init numStacks (fun _ -> []:char list)
+
+    let eoc = endOfConfig numStacks
+
+    let config = 
+        lines
+        |> List.takeWhile (fun l -> l.Equals(endOfConfig))
+        |> List.map readConfigLine
+    
--- 2022/Day05Tests.fs
+++ 2022/Day05Tests.fs
@@ -3,10 +3,51 @@ module Day05Tests
 open FsUnit.Xunit
 open Xunit
 
+open Utils
 open Day05
+
+open System.Text.RegularExpressions
 
 let testInput = Utils.readData "..\\..\\..\\Day05InputTest.txt"
 
 [<Fact>]
-let ``convert 1`` () =
-    ()
+let ``endOfConfig for 3 stacks`` () =
+    endOfConfig 3
+    |> should equal " 1   2   3 "
+
+[<Fact>]
+let ``endOfConfig for 9 stacks`` () =
+    endOfConfig 9
+    |> should equal " 1   2   3   4   5   6   7   8   9 "
+
+[<Fact>]
+let ``readConfigLine 1 for 3 stacks`` () =
+    readConfigLine "    [D]    "
+    |> should equal [|"";"D";""|]
+
+[<Fact>]
+let ``readConfigLine 2 for 3 stacks`` () =
+    readConfigLine "[Z] [M] [P]"
+    |> should equal [|"Z";"M";"P"|]
+    
+[<Fact>]
+let ``readConfigLine 3 for 3 stacks`` () =
+    readConfigLine "[N] [C]    "
+    |> should equal [|"N";"C";""|]
+
+
+[<Fact>]
+let ``readConfigLine for 9 stacks`` () =
+    readConfigLine "    [G] [N] [H] [S] [B]     [R] [F]"
+    |> should equal [|"";"G";"N";"H";"S";"B";"";"R";"F"|]
+
+[<Fact(Skip="ss")>]
+let ``initialConfigParser with test input`` () =
+    let expected = [|
+        ['N'; 'Z']
+        ['D'; 'C'; 'M']
+        ['P']
+    |]
+
+    initialConfigParser testInput 3
+    |> should equal expected
