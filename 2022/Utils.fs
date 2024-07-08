5e7563b54a638056d3365a9f492eee64d9128b25
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2024-07-08 17:31:18 +0200

    Download input from AoC website.

--- 2022/Utils.fs
+++ 2022/Utils.fs
@@ -1,6 +1,16 @@
 module Utils
 
+open FsHttp
 open System.Text.RegularExpressions
+
+let aoc_token="53616c7465645f5f11f1ae4157653608ee8c58b4a4c90b9cb9deb9c4e309ed179b38cc991e69cc916acad93e43e18f42ca25ebd9cd68cd85ac492981add30a39"
+let fetchDayInput day =
+    http {
+        GET $"https://adventofcode.com/2022/day/%d{day}/input"
+        Cookie "session" aoc_token
+    }
+    |> Request.send
+    |> Response.toText
 
 let readData filePath = 
     System.IO.File.ReadLines filePath
