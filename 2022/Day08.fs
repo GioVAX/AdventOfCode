459aba585532c50edccfef4593824821674598a4
Giovanni Pezzino <giovannipezzino.64@gmail.com>
2024-09-15 12:48:24 +0200

    Day08 - initial parsing

--- 2022/Day08.fs
+++ 2022/Day08.fs
@@ -1,3 +1,9 @@
 module Day08
 
 open Utils
+
+let inputTo2dArray (i:string) =
+    i.Split '\n'
+    |> Array.toList
+    |> List.map Seq.toList
+    |> array2D
--- 2022/Day08Tests.fs
+++ 2022/Day08Tests.fs
@@ -27,4 +27,28 @@ let ``input lines include the trailing \n`` () =
 [<Fact>]
 let ``input lines do not include leading spaces`` () =
     (input.Split '\n')[1]
-    |> String.exists ((=) ' ')
+    |> should not' (startWith " ")
+
+[<Fact>]
+let ``input is converted to the right 2d array`` () =
+    let expected = array2D [
+            ['3';'0';'3';'7';'3'];
+            ['2';'5';'5';'1';'2'];
+            ['6';'5';'3';'3';'2'];
+            ['3';'3';'5';'4';'9'];
+            ['3';'5';'3';'9';'0']]
+
+    let actual = 
+        input
+        |> inputTo2dArray
+    
+    actual |> should equal expected
+
+[<Fact>]
+let ``understanding slicing`` () =
+    let actual = 
+        input
+        |> inputTo2dArray
+
+    actual[0,*] |> should equal [|'3';'0';'3';'7';'3'|]
+    actual[*,1] |> should equal [|'0';'5';'5';'3';'5'|]
